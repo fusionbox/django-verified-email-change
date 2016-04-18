303de78f3c14df87713a20024c4ed38b09d27a3f
Julian Andrews <jandrews@fusionbox.com>
2016-04-18 16:39:40 -0600

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django-verified-email',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description='Verified email changes for django',
     long_description=long_description,
     license='BSD',
